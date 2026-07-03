19fd33bbcb11c7113c0b9ac00b67fdf0303bc967
Em-Jay1988 <michael_jock@web.de>
2026-07-03 16:22:10 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_URL = "https://d2runewizard.com/api/terror-zone"
+
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+
+LOCAL_TIMEZONE = "Europe/Berlin"
+
+APP_NAME = "D2 Companion"
+VERSION = "1.1"
