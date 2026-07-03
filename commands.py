0d37fec7d20b89b5270e9c7e3d6d2f49c757f650
Em-Jay1988 <michael_jock@web.de>
2026-07-03 15:36:49 +0200

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,21 @@
+from zones import ZONES
+
+
+def list_zones(user):
+    """Erzeugt die /list-Ausgabe."""
+    pass
+
+
+def watch_zone(user, zone_code):
+    """Aktiviert eine Terror Zone."""
+    pass
+
+
+def unwatch_zone(user, zone_code):
+    """Deaktiviert eine Terror Zone."""
+    pass
+
+
+def status():
+    """Zeigt aktuelle und nächste Terror Zone."""
+    pass
