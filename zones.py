75e1b8b74399a346c6f40fe94a130e1769ab2d4a
Em-Jay1988 <michael_jock@web.de>
2026-07-03 18:01:31 +0200

    Update zones.py

--- zones.py
+++ zones.py
@@ -1,81 +1,20 @@
 ZONES = {
-    # Akt I
-    "kata": {
-        "act": "⚔️ Akt I",
-        "name": "🕸️ Katakomben",
-        "keywords": ["cathedral", "catacombs"],
-    },
-    "pit": {
-        "act": "⚔️ Akt I",
-        "name": "🕳️ Die Grube",
-        "keywords": ["pit"],
-    },
+    "kata": {"act": "⚔️ Akt I", "name": "🕸️ Katakomben", "keywords": ["cathedral", "catacombs"]},
+    "pit": {"act": "⚔️ Akt I", "name": "🕳️ Die Grube", "keywords": ["pit"]},
 
-    # Akt II
-    "tal": {
-        "act": "🐪 Akt II",
-        "name": "🏜️ Tal Rashas Gräber",
-        "keywords": ["tal rasha"],
-    },
-    "at": {
-        "act": "🐪 Akt II",
-        "name": "🧊 Alte Tunnel",
-        "keywords": ["ancient tunnels"],
-    },
-    "arc": {
-        "act": "🐪 Akt II",
-        "name": "🌌 Arkane Zuflucht",
-        "keywords": ["arcane sanctuary"],
-    },
-    "maggot": {
-        "act": "🐪 Akt II",
-        "name": "🐛 Wurmgruft",
-        "keywords": ["maggot lair"],
-    },
+    "tal": {"act": "🏜️ Akt II", "name": "🏜️ Tal Rashas Gräber", "keywords": ["tal rasha"]},
+    "at": {"act": "🏜️ Akt II", "name": "🧊 Alte Tunnel", "keywords": ["ancient tunnels"]},
+    "arc": {"act": "🏜️ Akt II", "name": "🌌 Arkane Zuflucht", "keywords": ["arcane sanctuary"]},
+    "maggot": {"act": "🏜️ Akt II", "name": "🐛 Wurmgruft", "keywords": ["maggot lair"]},
 
-    # Akt III
-    "meph": {
-        "act": "🗿 Akt III",
-        "name": "💀 Kerker des Hasses",
-        "keywords": ["durance of hate"],
-    },
-    "trav": {
-        "act": "🗿 Akt III",
-        "name": "🏛️ Travincal",
-        "keywords": ["travincal"],
-    },
-    "flayer": {
-        "act": "🗿 Akt III",
-        "name": "🌿 Schinderdschungel",
-        "keywords": ["flayer jungle", "flayer dungeon"],
-    },
-    "kurast": {
-        "act": "🗿 Akt III",
-        "name": "🏯 Ober-Kurast",
-        "keywords": ["upper kurast", "kurast bazaar", "kurast causeway"],
-    },
+    "meph": {"act": "🌴 Akt III", "name": "💀 Kerker des Hasses", "keywords": ["durance of hate"]},
+    "trav": {"act": "🌴 Akt III", "name": "🏛️ Travincal", "keywords": ["travincal"]},
+    "kurast": {"act": "🌴 Akt III", "name": "🏯 Ober-Kurast", "keywords": ["upper kurast", "kurast bazaar", "kurast causeway"]},
+    "flayer": {"act": "🌴 Akt III", "name": "🌿 Schinderdschungel", "keywords": ["flayer jungle", "flayer dungeon"]},
 
-    # Akt IV
-    "chaos": {
-        "act": "🔥 Akt IV",
-        "name": "🔥 Chaos Sanktuarium",
-        "keywords": ["chaos sanctuary"],
-    },
+    "chaos": {"act": "🔥 Akt IV", "name": "🔥 Chaos Sanktuarium", "keywords": ["chaos sanctuary"]},
 
-    # Akt V
-    "wsk": {
-        "act": "👑 Akt V",
-        "name": "👑 Weltsteinturm",
-        "keywords": ["worldstone keep", "throne of destruction", "worldstone chamber"],
-    },
-    "nilly": {
-        "act": "👑 Akt V",
-        "name": "☠️ Nihlathaks Tempel",
-        "keywords": ["nihlathak", "halls of anguish", "halls of pain", "halls of vaught"],
-    },
-    "cows": {
-        "act": "👑 Akt V",
-        "name": "🐄 Kuhlevel",
-        "keywords": ["moo moo farm"],
-    },
+    "wsk": {"act": "👑 Akt V", "name": "👑 Weltsteinturm", "keywords": ["worldstone keep", "throne of destruction", "worldstone chamber"]},
+    "nilly": {"act": "👑 Akt V", "name": "☠️ Nihlathaks Tempel", "keywords": ["nihlathak", "halls of anguish", "halls of pain", "halls of vaught"]},
+    "cows": {"act": "👑 Akt V", "name": "🐄 Kuhlevel", "keywords": ["moo moo farm"]},
 }
