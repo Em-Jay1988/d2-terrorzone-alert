c2ffd241ee18424abd4b14e6daada13e73affa70
Em-Jay1988 <michael_jock@web.de>
2026-07-03 15:24:08 +0200

    Create zones.py

---
+++ zones.py
@@ -0,0 +1,18 @@
+ZONES = {
+    "kata": {"name": "🕸️ Katakomben", "keywords": ["cathedral", "catacombs"]},
+    "tal": {"name": "🏜️ Tal Rashas Gräber", "keywords": ["tal rasha"]},
+    "meph": {"name": "💀 Kerker des Hasses", "keywords": ["durance of hate"]},
+    "chaos": {"name": "🔥 Chaos Sanktuarium", "keywords": ["chaos sanctuary"]},
+    "wsk": {"name": "👑 Weltsteinturm", "keywords": ["worldstone", "throne of destruction", "worldstone chamber"]},
+
+    "at": {"name": "🧊 Alte Tunnel", "keywords": ["ancient tunnels"]},
+    "pit": {"name": "🕳️ Die Grube", "keywords": ["pit"]},
+    "cows": {"name": "🐄 Kuhlevel", "keywords": ["moo moo farm"]},
+    "arcane": {"name": "🌌 Arkane Zuflucht", "keywords": ["arcane sanctuary"]},
+    "trav": {"name": "🏛️ Travincal", "keywords": ["travincal"]},
+    "tombs": {"name": "🏜️ Felsengrab", "keywords": ["rocky waste", "stony tomb"]},
+    "nilly": {"name": "🦴 Nihlathaks Tempel", "keywords": ["nihlathak", "halls of anguish", "halls of pain", "halls of vaught"]},
+    "flayer": {"name": "🌿 Schinderdschungel", "keywords": ["flayer jungle", "flayer dungeon"]},
+    "tunnels": {"name": "🐍 Wurmgruft", "keywords": ["maggot lair", "far oasis"]},
+    "kurast": {"name": "🏯 Kurast", "keywords": ["kurast bazaar", "upper kurast", "sewers"]},
+}
