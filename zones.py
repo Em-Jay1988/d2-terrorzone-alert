bc97d39ae97fde598407b03ef9daab910aa30e9a
Em-Jay1988 <michael_jock@web.de>
2026-07-03 15:26:02 +0200

    Update zones.py

--- zones.py
+++ zones.py
@@ -1,18 +1,77 @@
 ZONES = {
-    "kata": {"name": "🕸️ Katakomben", "keywords": ["cathedral", "catacombs"]},
-    "tal": {"name": "🏜️ Tal Rashas Gräber", "keywords": ["tal rasha"]},
-    "meph": {"name": "💀 Kerker des Hasses", "keywords": ["durance of hate"]},
-    "chaos": {"name": "🔥 Chaos Sanktuarium", "keywords": ["chaos sanctuary"]},
-    "wsk": {"name": "👑 Weltsteinturm", "keywords": ["worldstone", "throne of destruction", "worldstone chamber"]},
+    # Boss-TZ
+    "kata": {
+        "name": "🕸️ Katakomben",
+        "keywords": ["cathedral", "catacombs"]
+    },
+    "tal": {
+        "name": "🏜️ Tal Rashas Gräber",
+        "keywords": ["tal rasha"]
+    },
+    "meph": {
+        "name": "💀 Kerker des Hasses",
+        "keywords": ["durance of hate"]
+    },
+    "chaos": {
+        "name": "🔥 Chaos Sanktuarium",
+        "keywords": ["chaos sanctuary"]
+    },
+    "wsk": {
+        "name": "👑 Weltsteinturm",
+        "keywords": [
+            "worldstone keep",
+            "throne of destruction",
+            "worldstone chamber"
+        ]
+    },
 
-    "at": {"name": "🧊 Alte Tunnel", "keywords": ["ancient tunnels"]},
-    "pit": {"name": "🕳️ Die Grube", "keywords": ["pit"]},
-    "cows": {"name": "🐄 Kuhlevel", "keywords": ["moo moo farm"]},
-    "arcane": {"name": "🌌 Arkane Zuflucht", "keywords": ["arcane sanctuary"]},
-    "trav": {"name": "🏛️ Travincal", "keywords": ["travincal"]},
-    "tombs": {"name": "🏜️ Felsengrab", "keywords": ["rocky waste", "stony tomb"]},
-    "nilly": {"name": "🦴 Nihlathaks Tempel", "keywords": ["nihlathak", "halls of anguish", "halls of pain", "halls of vaught"]},
-    "flayer": {"name": "🌿 Schinderdschungel", "keywords": ["flayer jungle", "flayer dungeon"]},
-    "tunnels": {"name": "🐍 Wurmgruft", "keywords": ["maggot lair", "far oasis"]},
-    "kurast": {"name": "🏯 Kurast", "keywords": ["kurast bazaar", "upper kurast", "sewers"]},
+    # Weitere beliebte Terrorzonen
+    "at": {
+        "name": "🧊 Alte Tunnel",
+        "keywords": ["ancient tunnels"]
+    },
+    "pit": {
+        "name": "🕳️ Die Grube",
+        "keywords": ["pit"]
+    },
+    "cows": {
+        "name": "🐄 Kuhlevel",
+        "keywords": ["moo moo farm"]
+    },
+    "arc": {
+        "name": "🌌 Arkane Zuflucht",
+        "keywords": ["arcane sanctuary"]
+    },
+    "trav": {
+        "name": "🏛️ Travincal",
+        "keywords": ["travincal"]
+    },
+    "nilly": {
+        "name": "☠️ Nihlathaks Tempel",
+        "keywords": [
+            "nihlathak",
+            "halls of anguish",
+            "halls of pain",
+            "halls of vaught"
+        ]
+    },
+    "flayer": {
+        "name": "🌿 Schinderdschungel",
+        "keywords": [
+            "flayer jungle",
+            "flayer dungeon"
+        ]
+    },
+    "maggot": {
+        "name": "🐛 Wurmgruft",
+        "keywords": ["maggot lair"]
+    },
+    "kurast": {
+        "name": "🏯 Ober-Kurast",
+        "keywords": [
+            "upper kurast",
+            "kurast bazaar",
+            "kurast causeway"
+        ]
+    },
 }
